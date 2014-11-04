de7f3c0a28f7ae3ed5115213180d77083ef314c8
Wayne K. Walrath <wayne@evolvingmedia.net>
2014-11-04 09:40:18 -0500

    Add wp_pagenavi to index.php

--- index.php
+++ index.php
@@ -15,9 +15,15 @@
 
 <?php if ($wp_query->max_num_pages > 1) : ?>
   <nav class="post-nav">
+    <div class="pager">
+      <?php wp_pagenavi(); ?>
+    </div>
+
+  <?php /*
     <ul class="pager">
       <li class="previous"><?php next_posts_link(__('&larr; Older posts', 'roots')); ?></li>
       <li class="next"><?php previous_posts_link(__('Newer posts &rarr;', 'roots')); ?></li>
     </ul>
+  */?>
   </nav>
 <?php endif; ?>
