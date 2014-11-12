416d527c57412a297efcb574b5dc3de45b8f9d18
Andre Malkine <andre@evolvingmedia.net>
2014-11-12 15:15:47 -0500

    Updates grunt tasks - default: dev build, dev: dev build + watch, build: dist build (minifies css, js)

--- Gruntfile.js
+++ Gruntfile.js
@@ -36,7 +36,7 @@ module.exports = function(grunt) {
       }
     },
     uglify: {
-      dist: {
+      dev: {
         files: {
           'assets/js/scripts.min.js': [
             'assets/js/plugins/bootstrap/transition.js',
@@ -61,6 +61,38 @@ module.exports = function(grunt) {
           sourceMap: 'assets/js/scripts.min.js.map',
           sourceMappingURL: '../../',
           beautify: true,
+          compress: {
+            //drop_console: true
+          },
+          //mangle: false,
+          preserveComments: true
+        }
+      },
+      dist: {
+        files: {
+          'assets/js/scripts.min.js': [
+            'assets/js/plugins/bootstrap/transition.js',
+            //'assets/js/plugins/bootstrap/alert.js',
+            'assets/js/plugins/bootstrap/button.js',
+            //'assets/js/plugins/bootstrap/carousel.js',
+            'assets/js/plugins/bootstrap/collapse.js',
+            'assets/js/plugins/bootstrap/dropdown.js',
+            //'assets/js/plugins/bootstrap/modal.js',
+            //'assets/js/plugins/bootstrap/tooltip.js',
+            //'assets/js/plugins/bootstrap/popover.js',
+            //'assets/js/plugins/bootstrap/scrollspy.js',
+            'assets/js/plugins/bootstrap/tab.js',
+            'assets/js/plugins/bootstrap/affix.js',
+            'assets/js/plugins/*.js',
+            'assets/js/_*.js',
+            'assets/js/vendor/jquery.html5-placeholder-shim.js'
+          ]
+        },
+        options: {
+          // JS source map: to enable, uncomment the lines below and update sourceMappingURL based on your install
+          sourceMap: 'assets/js/scripts.min.js.map',
+          sourceMappingURL: '../../',
+          beautify: false,
           compress: {
             //drop_console: true
           },
@@ -127,16 +159,18 @@ module.exports = function(grunt) {
   grunt.registerTask('default', [
     'clean',
     'sass',
-    'cssmin',
-    'uglify'
+    'uglify:dev'
     //,'version'
   ]);
   grunt.registerTask('dev', [
+    'default',
+    'watch'
+  ]);
+  grunt.registerTask('build', [
     'clean',
     'sass',
-    'uglify',
     'cssmin',
-    'watch'
+    'uglify:dist'
   ]);
 
 };
